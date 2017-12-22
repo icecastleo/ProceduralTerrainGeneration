ac4e696b66f8cb7dd50fc5ad8e31ed819d9f400e
FrozenIce <icecastle221@gmail.com>
2017-12-21 20:30:07 -0500

    Remove useless class. Add a rotating camera.

---
+++ Assets/Scripts/TrailerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerCamera : MonoBehaviour {
+
+    float speed = 10.0f;
+
+    Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+        float radius = (float)GetComponentInParent<TerrainGenerator>().mapWidth / 2.0f;
+        target = new Vector3(0.0f, -0.25f * radius, 0.0f);
+
+        transform.position = new Vector3(radius, 0.4f * radius, 0.0f);
+        transform.LookAt(target);
+    }
+	
+	void Update () {
+        transform.RotateAround(target, Vector3.up, Time.deltaTime * speed);
+    }
+}
